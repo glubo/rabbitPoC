8365e9e99ad6fc2ce8725fa6b44e353482b9de74
Petr Sykora <petr.sykora@gmail.com>
2024-01-14 15:10:51 +0100

    Add initial project files and configurations
    This commit adds various essential files for a new project setup, including license, source files, test cases, build scripts, and application properties. It also sets up Gradle, JDK, and Kotlin specific configurations needed for building the project. Moreover, it includes the setup for Micronaut framework and its associated features as well as the important JVM options.

---
+++ build.gradle.kts
@@ -0,0 +1,82 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.9.21"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.9.21"
+    id("com.google.devtools.ksp") version "1.9.21-1.0.16"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("io.micronaut.application") version "4.2.1"
+    id("gg.jte.gradle") version "3.0.3"
+    id("io.micronaut.aot") version "4.2.1"
+}
+
+version = "0.1"
+group = "cz.glubo"
+
+val kotlinVersion = project.properties.get("kotlinVersion")
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    ksp("io.micronaut:micronaut-http-validation")
+    ksp("io.micronaut.security:micronaut-security-annotations")
+    ksp("io.micronaut.serde:micronaut-serde-processor")
+    implementation("io.micronaut:micronaut-http-client")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-extension-functions")
+    implementation("io.micronaut.kotlin:micronaut-kotlin-runtime")
+    implementation("io.micronaut.security:micronaut-security-oauth2")
+    implementation("io.micronaut.serde:micronaut-serde-jackson")
+    implementation("io.micronaut.views:micronaut-views-jte")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:${kotlinVersion}")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:${kotlinVersion}")
+    runtimeOnly("ch.qos.logback:logback-classic")
+    runtimeOnly("com.fasterxml.jackson.module:jackson-module-kotlin")
+    aotPlugins(platform("io.micronaut.platform:micronaut-platform:4.2.3"))
+    aotPlugins("io.micronaut.security:micronaut-security-aot")
+}
+
+
+application {
+    mainClass.set("cz.glubo.ApplicationKt")
+}
+java {
+    sourceCompatibility = JavaVersion.toVersion("17")
+}
+
+
+graalvmNative.toolchainDetection.set(false)
+micronaut {
+    runtime("netty")
+    testRuntime("kotest5")
+    processing {
+        incremental(true)
+        annotations("cz.glubo.*")
+    }
+    aot {
+        // Please review carefully the optimizations enabled below
+        // Check https://micronaut-projects.github.io/micronaut-aot/latest/guide/ for more details
+        optimizeServiceLoading.set(false)
+        convertYamlToJava.set(false)
+        precomputeOperations.set(true)
+        cacheEnvironment.set(true)
+        optimizeClassLoading.set(true)
+        deduceEnvironment.set(true)
+        optimizeNetty.set(true)
+        configurationProperties.put("micronaut.security.jwks.enabled", "false")
+        configurationProperties.put("micronaut.security.openid-configuration.enabled", "false")
+    }
+}
+
+
+jte {
+    sourceDirectory.set(file("src/main/jte").toPath())
+    generate()
+}
+
+// Gradle requires that generateJte is run before some tasks
+tasks.configureEach {
+    if (name == "inspectRuntimeClasspath") {
+        mustRunAfter("generateJte")
+    }
+}
+
+
---
+++ src/test/kotlin/cz/glubo/RabbitpocTest.kt
@@ -0,0 +1,13 @@
+package cz.glubo
+
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.kotest5.annotation.MicronautTest
+import io.kotest.core.spec.style.StringSpec
+
+@MicronautTest
+class RabbitpocTest(private val application: EmbeddedApplication<*>) : StringSpec({
+
+    "test the server is running" {
+        assert(application.isRunning)
+    }
+})
